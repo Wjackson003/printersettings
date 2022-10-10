1883ca215e46d95bbc5f47d37174b0c53c36a440
WT_JacksonIII <wjackson@indyme.com>
2022-10-10 09:52:49 -0700

    This was to change the search of file path

--- testfile.py
+++ testfile.py
@@ -1,10 +1,16 @@
 '''
     this is a demo how to use the workspaceip code. 
+
+    version 0.1.0 Oct 10, 2022: changes made for relative path to be found versus an absolute path
 '''    
+__Author__ = "W.T. Jackson III"
+__Version__ = "0.1.0"
 
 from workspaceip import GetPrintSet
+import os
 
 
 nGetPrintSet = GetPrintSet()
-sfile = '/home/pi/Python_Code/ipset/ipsettings/printServer.ini'
+dirname = os.path.dirname(__file__)
+sfile = os.path.join(dirname, 'printServer.ini')
 nGetPrintSet.checkforfile(sfile)
